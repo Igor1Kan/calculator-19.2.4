74387e3ca06dd4287d80677d3d06687ae8026015
Anton Abazin <anton@fly7.ru>
2023-07-20 16:11:00 +0300

    tests

--- test_calculator.py
+++ test_calculator.py
@@ -41,3 +41,34 @@ def test_button_equal(calculator):
     calculator.number_entry.insert(0, "7")
     calculator.equal()
     assert calculator.number_entry.get() == "12"
+
+
+def test_button_subtract(calculator):
+    calculator.number_entry.insert(0, "10")
+    calculator.subtract()
+    assert calculator.math == "subtraction"
+    assert calculator.f_num == 10
+    assert calculator.number_entry.get() == ""
+
+
+def test_button_multiply(calculator):
+    calculator.number_entry.insert(0, "6")
+    calculator.multiply()
+    assert calculator.math == "multiplication"
+    assert calculator.f_num == 6
+    assert calculator.number_entry.get() == ""
+
+
+def test_button_divide(calculator):
+    calculator.number_entry.insert(0, "20")
+    calculator.divide()
+    assert calculator.math == "division"
+    assert calculator.f_num == 20
+    assert calculator.number_entry.get() == ""
+
+def test_button_floor_div(calculator):
+    calculator.number_entry.insert(0, "15")
+    calculator.floor_div()
+    assert calculator.math == "floor_div"
+    assert calculator.f_num == 15
+    assert calculator.number_entry.get() == ""
